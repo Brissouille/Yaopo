d2f9a5a32462e366485a7a48ba4b67c443cfb603
Valliere Brice <valliere.brice@gmail.com>
2026-03-22 14:37:51 +0100

    Add comments related to Optee

--- tee_interface.h
+++ tee_interface.h
@@ -7,4 +7,31 @@ struct tee_ctx
     //TODO
 };
 
+#if 0
+TEEC_Result TEEC_InitializeContext(
+    const char* name,
+    TEEC_Context* context)
+
+void TEEC_FinalizeContext(
+    TEEC_Context* context)
+
+TEEC_Result TEEC_OpenSession (
+    TEEC_Context* context,
+    TEEC_Session* session,
+    const TEEC_UUID* destination,
+    uint32_t connectionMethod,
+    const void* connectionData,
+    TEEC_Operation* operation,
+    uint32_t* returnOrigin)
+
+void TEEC_CloseSession (
+    TEEC_Session* session)
+
+TEEC_Result TEEC_InvokeCommand(
+    TEEC_Session* session,
+    uint32_t commandID,
+    TEEC_Operation* operation,
+    uint32_t* returnOrigin)
+#endif
+
 #endif // TEE_INTERFACE_H
